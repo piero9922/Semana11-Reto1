4a2aa85e9290cd8bcbb91662f4c3fb655bb87095
Piero Montalvo <piero.montalvo9922@gmail.com>
2020-06-13 10:29:03 -0500

    reto1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let submit = document.getElementById('entrada');
+let num = document.getElementsByClassName('numbers')
+
+main.addEventListener('click',(event) =>{
+    let contenido = event.target.textContent
+    console.log(contenido)
+})
